936dcbc70791b661d8bff64ce7d8a06496949714
Gilmore Davidson <gilmoreorless@gmail.com>
2014-11-25 18:35:50 +1100

    Tangle-less version

--- proto.js
+++ proto.js
@@ -1,54 +1,81 @@
 (function () {
 
-    Tangle.classes.ECLinkedValue = {
-        initialize: function (elem, options, tangle, variable) {
-            console.log('ECLinkedValue.initialize()', arguments);
-            this.previous = options.previous;
-            this.tangle = tangle;
-
-            elem.addEventListener('input', function () {
+    function initInputs(box) {
+        box.propList.forEach(function (prop) {
+            var elem = box.inputs[prop] = box.root.querySelector('input[data-prop=' + prop + ']');
+            elem.oninput = function () {
                 var value = elem.value;
                 console.log('elem.oninput()', value, elem);
-                tangle.setValue(variable, value);
-            }, false);
-        },
-        update: function (elem, value, fallbackValue, superFallbackValue) {
-            console.log('ECLinkedValue.update()', arguments);
-            if (elem.value !== value) {
-                elem.value = value;
-            }
-            elem.classList[this.isEmpty(value) ? 'add' : 'remove']('empty');
+                box.setValue(prop, value);
+            };
+        });
+    }
 
-            // Make sure parent has a value
-            if (this.previous && !this.isEmpty(value)) {
-                var previousValue = this.tangle.getValue(this.previous);
-                if (this.isEmpty(previousValue)) {
-                    this.tangle.setValue(this.previous, 0);
-                }
-            }
-        },
-        isEmpty: function (value) {
-            return value === '';
+
+    function BoxModel(root, property) {
+        this.root = root;
+        this.example = root.querySelector('.box-model');
+        this.property = property;
+        this.values = {
+            top: '1em',
+            right: '',
+            bottom: '',
+            left: ''
+        };
+        this.propList = ['top', 'right', 'bottom', 'left']; // Maintain exact order
+        this.inputs = {};
+        initInputs(this);
+        this.update();
+    }
+
+    BoxModel.isEmpty = function (value) {
+        return value === '';
+    };
+
+    BoxModel.prototype.each = function (callback) {
+        this.propList.forEach(function (prop) {
+            callback.call(this, prop, this.values[prop], this.inputs[prop]);
+        }, this);
+    };
+
+    BoxModel.prototype.setValue = function (prop, value) {
+        var oldValue = this.values[prop];
+        console.log('BoxModel.setValue(%s):', prop, value);
+        if (oldValue !== value) {
+            this.values[prop] = value;
+            this.update();
         }
     };
 
-    var root = document.getElementById('shorthand');
-    var example = root.querySelector('.box-model');
+    BoxModel.prototype.update = function () {
+        console.log('BoxModel.update()');
+        this.enforceValidInput();
+        this.each(function (prop, value, elem) {
+            if (elem.value !== value) {
+                elem.value = value;
+            }
+            elem.classList[BoxModel.isEmpty(value) ? 'add' : 'remove']('empty');
+        });
+        this.example.style[this.property] = [this.values.top, this.values.right, this.values.bottom, this.values.left].join(' ');
+    };
 
-    var tangle = new Tangle(root, {
-        initialize: function () {
-            console.log('Tangle.initialize()', arguments);
-            this.top = '1em';
-            this.right = '';
-            this.bottom = '';
-            this.left = '';
-        },
-        update: function () {
-            console.log('Tangle.update()', arguments);
-            example.style.padding = [this.top, this.right, this.bottom, this.left].join(' ');
+    BoxModel.prototype.enforceValidInput = function () {
+        var hasValue = false;
+        var prop, elem;
+        var i = this.propList.length;
+        while (i--) {
+            prop = this.propList[i];
+            elem = this.inputs[prop];
+            if (BoxModel.isEmpty(elem.value)) {
+                if (hasValue) {
+                    this.values[prop] = 0;
+                }
+            } else {
+                hasValue = true;
+            }
         }
-    });
+    };
 
-    window.tangle = tangle;
+    window.paddingBox = new BoxModel(document.getElementById('shorthand'), 'padding');
 
 })();
